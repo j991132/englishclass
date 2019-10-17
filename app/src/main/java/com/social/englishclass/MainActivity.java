99ee9471d9ddf1b261d660e588bf812e97f99fc2
j991132 <j991132@naver.com>
2019-10-17 12:45:32 +0900

    1016 오디오 불러오기중

---
+++ app/src/main/java/com/social/englishclass/MainActivity.java
@@ -0,0 +1,19 @@
+package com.social.englishclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, englishlesson.class);
+//        intent.putExtra("ename", String.valueOf(ename.getText()));
+        startActivity(intent);
+    }
+}
