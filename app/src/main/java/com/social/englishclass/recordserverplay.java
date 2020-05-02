7ccaaab5b7c92d8dfe59998edf39266e12e78d5b
j991132 <j991132@gmail.com>
2020-05-02 22:53:51 +0900

    0502 피드백 액티비티 구성중(선생님과 학생을 어떻게 구분할까)

---
+++ app/src/main/java/com/social/englishclass/recordserverplay.java
@@ -0,0 +1,14 @@
+package com.social.englishclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recordserverplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recordserverplay);
+    }
+}
