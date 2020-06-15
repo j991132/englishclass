3d820db4237a188b97064650817b0e0958c238b6
j991132 <j991132@naver.com>
2020-06-15 16:08:19 +0900

    0615 서버 플레이 수정

--- app/src/main/java/com/social/englishclass/recordserverplay.java
+++ app/src/main/java/com/social/englishclass/recordserverplay.java
@@ -528,15 +528,16 @@ databaseReference.child("umd_test").child(filename).addChildEventListener(new Ch
                         Log.e("다운로드받은 녹음파일 ", "" + downfile);
                         Log.e("레슨타입 let 가 아닐때  녹음파일 ", "" + filepath);
  //웨이브곡선 프래그먼트
-                            if(filepath.contains("ll") || filepath.contains("ls") || filepath.contains("lr") || filepath.equals("") ){
+                        if(filepath.contains("_let_p") ){
+//                            if(filepath.contains("ll") || filepath.contains("ls") || filepath.contains("lr") || filepath.equals("") ){
 //                                wave_fragment_layer.setVisibility(View.GONE);
                                 getSupportFragmentManager().beginTransaction()
-
+                                        .add(R.id.container, new CustomWaveformFragment2())
                                         .add(R.id.container2, new CustomWaveformFragment())
                                         .commit();
                             }else {
                                 getSupportFragmentManager().beginTransaction()
-                                        .add(R.id.container, new CustomWaveformFragment2())
+
                                         .add(R.id.container2, new CustomWaveformFragment())
                                         .commit();
                             }
